5a12a0d83370ae5b863069fe08bd7820bfe7024d
Emlyn <emlynevans00@gmail.com>
2021-04-08 16:15:08 +1000

    Added skeleton for prescribed functions

--- main.py
+++ main.py
@@ -1,1 +1,40 @@
-print("Hello, World!")
+
+def utility(state):
+
+    # TODO
+
+    # if red is winner, return 10000
+    # if yellow is winner, return -10000
+
+    return
+
+
+def evaluation(state):
+
+    # TODO
+
+    # eval = score(state, red player) - score(state, yellow player)
+    # return eval
+
+
+    return
+
+
+def score(state, player):
+
+    # TODO
+
+    # score = number of player tokens
+    # score += 10 * num_in_a_row(2, state, player)
+    # score += 100 * num_in_a_row(3, state, player)
+    # score += 1000 * num_in_a_row(4 or more, state, player)
+    # return score
+
+    return
+
+
+def num_in_a_row(count, state, player):
+
+    # return the number of times there exists count-in-a-row for player in state
+
+    return
