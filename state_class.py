3f2df376dff4c26fd9c02bc01ed65e194234fa60
Emlyn <emlynevans00@gmail.com>
2021-04-08 17:57:25 +1000

    Added description of algorithm for board traversal

--- state_class.py
+++ state_class.py
@@ -135,6 +135,24 @@ class State:
 
     def num_in_a_row(self, count, player):
 
-        # return the number of times there exists count-in-a-row for player in state
+        # return the number of times there exists count-in-a-row for player in
+        # state
+        
+
+        # When checking a coordinate row-col, we first check horizontally. We
+        # track two variables, the first being the number of same tokens to the
+        # left (or right) as well as if the token to the left is empty or not.
+
+        # We can then add the left and right together.
+        # WE can also check if the left or right was empty, we don't need to
+        # check the upper diagonals as no token can exist there.
+
+        # There has to be some sort of iterative solution we can build. Maybe we
+        # track the left and upper slots. AS we move right, we continually add
+        # to the horizontal count until we hit a blank or other coloured token.
+        # As we move up, we add until we hit nothing or the other token. When we
+        # do, we count the number in a row.
+
+        # So we set/check 4 values: right, upper right, upper, upper left.
 
         return
