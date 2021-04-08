b788cee527e6a790f3cd4b6a5abcb9c33ebcd54a
Emlyn <emlynevans00@gmail.com>
2021-04-08 16:22:57 +1000

    + Collecting command line arguments

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello, World!")
+
+import sys
+
+board_str = sys.argv[1]
+player_turn = sys.argv[2]
+algorithm = sys.argv[3]
+max_depth = int(sys.argv[4])
+
+print(f"Board: {board_str}")
+print(f"Player turn: {player_turn}")
+print(f"Algorithm type: {algorithm}")
+print(f"Max depth of search: {max_depth}")
