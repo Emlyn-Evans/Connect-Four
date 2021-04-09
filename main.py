df5b50c08256fea3238645853a667aa68634e796
Emlyn <emlynevans00@gmail.com>
2021-04-10 00:46:21 +1000

    Minor edits for debug statements

--- main.py
+++ main.py
@@ -2,83 +2,19 @@ import sys
 from state_class import Board, State, Node
 
 
-def print_arguments():
+# def print_arguments():
 
-    board_str = sys.argv[1]
-    player_turn = sys.argv[2]
-    algorithm = sys.argv[3]
-    max_depth = int(sys.argv[4])
+#     board_str = sys.argv[1]
+#     player_turn = sys.argv[2]
+#     algorithm = sys.argv[3]
+#     max_depth = int(sys.argv[4])
 
-    print(f"Board: {board_str}")
-    print(f"Player turn: {player_turn}")
-    print(f"Algorithm type: {algorithm}")
-    print(f"Max depth of search: {max_depth}")
+#     print(f"Board: {board_str}")
+#     print(f"Player turn: {player_turn}")
+#     print(f"Algorithm type: {algorithm}")
+#     print(f"Max depth of search: {max_depth}")
 
-    return
-
-
-def adjust_state(state):
-
-    # Adjust board:
-    state.board.add_move(0, "X")
-    state.board.add_move(1, "X")
-    state.board.add_move(2, "X")
-    state.board.add_move(3, "X")
-    state.board.add_move(4, "X")
-    state.board.add_move(5, "X")
-    state.board.add_move(6, "X")
-    print(state.board)
-    print(state.board.get_filled_cols())
-
-    state.board.add_move(2, "O")
-    state.board.add_move(2, "O")
-    print(state.board)
-    print(state.board.get_filled_cols())
-
-    return
-
-
-def example_evaluation():
-
-    board_str = "..yyrrr,..ryryr,....y..,.......,.......,......."
-    state = State(board_str)
-
-    print(state.board)
-    print(state.board.get_filled_cols())
-
-    state.compute_evaluation()
-    state.print_evaluation()
-
-    return
-
-
-def generate_children():
-
-    board_str = "yryrrry,.rryyrr,.yyyrr.,.ryy.y.,...r...,......."
-    max_depth = 5
-    last_move = "X"
-
-    state = State(board_str)
-    head = Node(state)
-    head.last_move = last_move
-
-    print(head.state.board)
-    head.state.compute_evaluation()
-    head.state.print_evaluation()
-
-    # Creating children
-    head.generate_children()
-
-    print("Original")
-    print(head.state.board)
-    head.state.print_evaluation()
-
-    for i in range(len(head.children)):
-
-        print(f"Child: {i}")
-        print(head.children[i].state.board)
-        print(head.children[i].state.print_evaluation())
-        print("\n")
+#     return
 
 
 def recursively_generate_children(node, depth, max_depth):
@@ -115,13 +51,16 @@ def dfs_print(node, depth, max_depth):
 
 def compute_minimax(node, depth, max_depth):
 
-    if node.state.utility != 0:
+    # if node.state.utility != 0:
 
-        return node.state.utility
+    #     return node.state.utility
 
     if depth == max_depth:
 
         node.minimax = node.state.evaluation
+
+        print(f"Node: {node.name} has minimax: {node.minimax}")
+
         return node.minimax
 
     if node.last_move == "O":
@@ -133,10 +72,10 @@ def compute_minimax(node, depth, max_depth):
 
             ret = compute_minimax(node.children[i], depth + 1, max_depth)
 
-            if depth == 0:
-                print(
-                    f"MAX: {node.children[i].name}: {ret} for child {i} in depth {depth}"
-                )
+            # if depth == 0:
+            #     print(
+            #         f"MAX: {node.children[i].name}: {ret} for child {i} in depth {depth}"
+            #     )
 
             if alpha is None:
 
@@ -148,9 +87,12 @@ def compute_minimax(node, depth, max_depth):
                 alpha = ret
                 alpha_index = i
 
-        print(f"Taking max: {alpha} in depth {depth}")
+        # print(f"Taking max: {alpha} in depth {depth}")
         node.minimax = alpha
         node.minimax_index = alpha_index
+        print(
+            f"Taking MAX: Node: {node.name} has minimax: {node.minimax} from child {node.minimax_index}"
+        )
 
     else:
 
@@ -161,10 +103,13 @@ def compute_minimax(node, depth, max_depth):
 
             ret = compute_minimax(node.children[i], depth + 1, max_depth)
 
-            if depth == 1:
-                print(
-                    f"MIN: {node.children[i].name}: {ret} for child {i} in depth {depth}"
-                )
+            # if depth == 1:
+            # print(
+            #     f"MIN: {node.children[i].name}: {ret} for child {i} in depth {depth}"
+            # )
+
+            if ret is None:
+                print(f"Something wrong at node: {node.name} child: {i}")
 
             if beta is None:
 
@@ -176,10 +121,14 @@ def compute_minimax(node, depth, max_depth):
                 beta = ret
                 beta_index = i
 
-        print(f"Taking min: {beta} in depth {depth}")
+        # print(f"Taking min: {beta} in depth {depth}")
 
         node.minimax = beta
         node.minimax_index = beta_index
+
+        print(
+            f"Taking MIN: Node: {node.name} has minimax: {node.minimax} from child {node.minimax_index}"
+        )
 
     return node.minimax
 
@@ -187,10 +136,10 @@ def compute_minimax(node, depth, max_depth):
 def set_up_state():
 
     # Inputs
-    # board_str = "yryrrry,.rry.r.,.yyy.r.,.ryy.y.,...r...,......."
+    board_str = "yryrrry,.rry.r.,.yyy.r.,.ryy.y.,...r...,......."
     # board_str = ".yrry..,..ryr..,..yry..,.......,.......,......."
-    board_str = ".......,.......,.......,.......,.......,......."
-    max_depth = 5
+    # board_str = ".......,.......,.......,.......,.......,......."
+    max_depth = 4
     last_move = "O"
 
     # Initialise
@@ -201,6 +150,8 @@ def set_up_state():
     recursively_generate_children(head, 0, max_depth)
 
     compute_minimax(head, 0, max_depth)
+
+    print("\n\n----------------\n")
 
     current_node = head
 
