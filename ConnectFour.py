f42c2d58de1768292ce1f54c8e5dde3fdc4f6ac2
Emlyn <emlynevans00@gmail.com>
2021-04-12 01:17:49 +1000

    + Bitstrings column-wise, adding and removing moves

--- ConnectFour.py
+++ ConnectFour.py
@@ -13,13 +13,35 @@ def init(board_str, turn):
 
         move = "O"
 
-    board = Board()
+    board = Board(move)
     board.create_board_from_str(board_str)
 
-    print(f"X Bits: {board.x_bits} : Number: {board.x_num}")
-    print(f"O Bits: {board.o_bits} : Number: {board.o_num}")
+    print(
+        f"X bits: {board.get_bin(board.get_player_pos('X'))} : Number: {board.get_player_pos('X')}"
+    )
+    print(
+        f"O bits: {board.get_bin(board.get_player_pos('O'))} : Number: {board.get_player_pos('O')}"
+    )
 
     print(board)
+    print(f"Player turn: {board.player}")
+    print(f"Win for X: {board.check_win('X')}")
+    print(f"Win for O: {board.check_win('O')}")
+
+    col = 5
+    print(f"Adding move to col {col}")
+
+    board.add_move(col)
+
+    print(board)
+    print(f"Player turn: {board.player}")
+
+    print(f"Removing move")
+
+    board.remove_move(col)
+
+    print(board)
+    print(f"Player turn: {board.player}")
 
     return
 
--- classes.py
+++ classes.py
@@ -1,79 +1,200 @@
 class Board:
-    def __init__(self):
+    def __init__(self, player):
 
         self.rows = 6
         self.cols = 7
-        self.x_bits = ""
-        self.o_bits = ""
-        self.x_num = None
-        self.o_num = None
+        self.player = player
+        self.pos = None
+        self.board = None
 
     def create_board_from_str(self, board_str):
 
+        player_bits = ""
+        board_bits = ""
+
         rows = board_str.split(",")
 
-        for i in range(self.rows - 1, -1, -1):
+        for j in range(self.cols - 1, -1, -1):
 
-            self.x_bits += "0"
-            self.o_bits += "0"
+            player_bits += "0"
+            board_bits += "0"
 
-            for j in range(self.cols - 1, -1, -1):
+            for i in range(self.rows - 1, -1, -1):
 
                 if rows[i][j] == "r":
 
-                    self.x_bits += "1"
-                    self.o_bits += "0"
+                    player_bits += "1"
+                    board_bits += "1"
 
                 elif rows[i][j] == "y":
 
-                    self.x_bits += "0"
-                    self.o_bits += "1"
+                    player_bits += "0"
+                    board_bits += "1"
 
                 else:
 
-                    self.x_bits += "0"
-                    self.o_bits += "0"
+                    player_bits += "0"
+                    board_bits += "0"
 
-        self.x_num = int(self.x_bits, 2)
-        self.o_num = int(self.o_bits, 2)
+        self.pos = int(player_bits, 2)
+        self.board = int(board_bits, 2)
 
         return
 
-    def add_move(self, col, char):
+    def check_win(self, player):
+
+        num = None
+
+        if player == self.player:
+
+            num = self.pos
+
+        else:
+
+            num = self.board ^ self.pos
+
+        # Horizontal
+        mask = num & num >> 7
+        result = mask & mask >> 14
+
+        if result != 0:
+
+            return True
+
+        # Vertical
+        mask = num & num >> 1
+        result = mask & mask >> 2
+
+        if result != 0:
+
+            return True
+
+        # Major diagonal
+        mask = num & num >> 6
+        result = mask & mask >> 12
+
+        if result != 0:
+
+            return True
+
+        # Minor diagonal
+        mask = num & num >> 8
+        result = mask & mask >> 16
+
+        if result != 0:
+
+            return True
+
+        return False
+
+    def add_move(self, col):
 
         # Check if legal move
+        if self.check_filled(col) == 0:
+
+            # Switch player and add move to board
+            self.switch_player()
+            self.board = self.board | self.board + (1 << (7 * col))
+
+        else:
+
+            print("INVALID MOVE IN FILLED COLUMN")
 
         return
 
     def remove_move(self, col):
 
+        # If the board has a move in the column
+        if int(self.get_bin(self.board)[self.coord_to_index(0, col)]):
+
+            # If that move is the current player move
+            if int(self.get_bin(self.pos)[self.coord_to_index(0, col)]):
+
+                self.switch_player()
+
+            # Undo board move
+            self.board = self.board & self.board - (1 << (7 * col))
+            self.switch_player()
+
+        else:
+
+            print("INVALID RE-MOVE IN EMPTY COLUMN")
+
         return
+
+    def switch_player(self):
+
+        # Flip the pos
+        self.pos = self.get_player_pos(self.player) ^ self.board
+
+        if self.player == "X":
+
+            self.player = "O"
+
+        else:
+
+            self.player = "X"
+
+        return
+
+    def check_filled(self, col):
+
+        index = self.coord_to_index(self.rows - 1, col)
+        ret = int(self.get_bin(self.board)[index])
+
+        return ret
+
+    def coord_to_index(self, row, col):
+
+        index = len(self.get_bin(self.board)) - 1 - ((col * (self.rows + 1)) + row)
+
+        return index
+
+    def get_player_pos(self, player):
+
+        ret = None
+
+        if player == self.player:
+
+            ret = self.pos
+
+        else:
+
+            ret = self.board ^ self.pos
+
+        return ret
+
+    def get_bin(self, num):
+
+        return f"{num:049b}"
 
     def __str__(self):
 
         ret = "\n---------------\n"
 
-        offset = 0
-
-        for i in range(self.rows):
-
-            offset = (i + 1) * 8 - 1
+        for i in range(self.rows - 1, -1, -1):
 
             for j in range(self.cols):
 
-                if self.x_bits[offset] == "1":
+                if self.get_bin(self.board)[self.coord_to_index(i, j)] == "1":
 
-                    ret += " X"
+                    if self.get_bin(self.pos)[self.coord_to_index(i, j)] == "1":
 
-                elif self.o_bits[offset] == "1":
+                        ret += f" {self.player}"
 
-                    ret += " O"
+                    else:
+
+                        if self.player == "X":
+
+                            ret += " O"
+
+                        else:
+
+                            ret += " X"
 
                 else:
 
                     ret += " ."
-
-                offset -= 1
 
             ret += "\n"
 
